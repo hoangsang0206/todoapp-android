767525e35d8a89374cc69757794eb40a4f72d3e4
Sang <lehoangsang02062003@gmail.com>
2024-05-08 19:57:06 +0700

    Add config firebase realtime database

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,16 +1,69 @@
 package com.example.todoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txt;
+    Button btnSend;
+    TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnSend = findViewById(R.id.btnSend);
+        txt = findViewById(R.id.txtTest);
+        txtView = findViewById(R.id.txtView);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = txt.getText().toString();
+                pushData(str);
+            }
+        });
+
+        getData();
+    }
+
+    private void pushData(String str) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("test");
+
+        myRef.setValue(str);
+    }
+
+    private void getData() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("test");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                txtView.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
